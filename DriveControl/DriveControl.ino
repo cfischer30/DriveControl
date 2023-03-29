e9663e7e5c2479371d361a2b35b163b9b69f7ddb
cfischer30 <84738691+cfischer30@users.noreply.github.com>
2023-03-29 16:04:46 -0400

    Initial Commit
    Initial Commit

---
+++ DriveControl/DriveControl.ino
@@ -0,0 +1,83 @@
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+
+const int MPU = 0x68; // MPU6050 I2C address
+float AccX, AccY, AccZ; //linear acceleration
+float GyroX, GyroY, GyroZ; //angular velocity
+float accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ; //used in void loop()
+float roll, pitch, yaw;
+float AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ;
+float elapsedTime, currentTime, previousTime;
+int c = 0;
+
+const int maxSpeed = 150; //max PWM value written to motor speed pin. It is typically 255.
+const int minSpeed = 80; //min PWM value at which motor moves
+float angle; //due to how I orientated my MPU6050 on my car, angle = roll
+float targetAngle = 0;
+int targetSpeed = 180;
+float angleTolerance = 5;
+
+const int left1 = 4;
+const int left2 = 5;
+const int right2 = 6;
+const int right1 = 7;
+const int leftSpeed = 10;
+const int rightSpeed = 11;
+
+int leftSpeedVal, rightSpeedVal;
+
+#include "gyro.h"
+#include "movement.h"
+
+const int buttonPin = 8;
+
+
+
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();                      // Initialize comunication
+  Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
+  Wire.write(0x6B);                  // Talk to the register 6B
+  Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
+  Wire.endTransmission(true);        //end the transmission
+  // Call this function if you need to get the IMU error values for your module
+  calculateError();
+
+  pinMode(buttonPin,INPUT);
+
+}
+
+void loop() {
+  long int time0, timeStart, timeNow;
+  // put your main code here, to run repeatedly:
+  if (digitalRead(buttonPin)==HIGH){
+    Serial.println("Button Pushed");
+    delay(1000);
+    targetAngle = 45;
+    Serial.print("CurrentAngle =");Serial.println(angle);
+    Serial.print("TargetAngle = ");Serial.println(targetAngle);
+    timeStart = millis();
+    timeNow = millis();
+    rightSpeedVal = targetSpeed;
+    leftSpeedVal = targetSpeed;
+    while((timeNow-timeStart) < 10000){
+      //Serial.print("TimeS, TimeN = ");Serial.print(timeStart); Serial.print("  "); Serial.println(timeNow);
+      moveControl();
+      timeNow = millis();
+    }
+    stopCar();
+        
+      
+    
+  }  
+
+  
+
+
+  
+}
+
+
---
+++ DriveControl/movement.h
@@ -0,0 +1,157 @@
+/*   Available functions
+
+driving() - manages wheel speed while driving forward
+controlSpeed() - adjusts correction speed differential proportional to delta angle.  Called by driving()
+rotate() - rotates robot when not moving
+changeSpeed() - manages minimum and maximum PWM settings
+
+
+
+*/
+void forward();
+void left();
+void right();
+void driving();
+void controlSpeed();
+void rotate();
+int changeSpeed (int motorSpeed, int increment);
+void forward();
+void left();
+void right();
+void stopCar();
+void moveControl();
+
+
+void moveControl(){
+  readAcceleration();
+  previousTime = currentTime;
+  currentTime = micros();
+  elapsedTime = (currentTime - previousTime) / 1000000;
+  readGyro();
+  GyroX -= GyroErrorX;
+  GyroY -= GyroErrorY;
+  GyroZ -= GyroErrorZ;
+  yaw += GyroZ * elapsedTime;
+  angle = yaw;
+  Serial.print("Curent Angle = ");Serial.println(angle);
+  driving();
+  }
+
+
+
+
+void driving (){//called by void loop(), which isDriving = true
+  int deltaAngle = round(targetAngle - angle); //rounding is neccessary, since you never get exact values in reality
+  Serial.print("deltaAngle = ");Serial.println(deltaAngle);  
+  forward();  // sets direction
+  //if (deltaAngle != 0){
+    if (deltaAngle > angleTolerance){
+   
+    Serial.print("before left right speed "); 
+    Serial.print(leftSpeedVal); Serial.print(" "); Serial.println(rightSpeedVal);    
+    controlSpeed ();
+   // rightSpeedVal = maxSpeed;
+    Serial.print("before left right speed "); 
+    Serial.print(leftSpeedVal); Serial.print(" "); Serial.println(rightSpeedVal);    
+    
+    analogWrite(rightSpeed, rightSpeedVal);
+    analogWrite(leftSpeed, leftSpeedVal);
+  }
+}
+
+void controlSpeed(){//this function is called by driving ()
+  int deltaAngle = round(targetAngle - angle);
+  int targetGyroZ;
+  
+  //setting up propoertional control, see Step 3 on the website
+  if (deltaAngle > 30){
+      targetGyroZ = 60;
+  } else if (deltaAngle < -30){
+    targetGyroZ = -60;
+  } else {
+    targetGyroZ = 2 * deltaAngle;
+  }
+  
+  if (round(targetGyroZ - GyroZ) == 0){
+    ;
+  } else if (targetGyroZ > GyroZ){
+    leftSpeedVal = changeSpeed(leftSpeedVal, -1); //would increase GyroX
+  } else {
+    leftSpeedVal = changeSpeed(leftSpeedVal, +1);
+  }
+}
+
+void rotate (){//called by void loop(), which isDriving = false
+  int deltaAngle = round(targetAngle - angle);
+  Serial.print("deltaAngle = "); Serial.println(deltaAngle);
+  int targetGyroZ;
+  if (abs(deltaAngle) <= 1){
+    stopCar();
+  } else {
+    if (angle > targetAngle) { //turn left
+      left();
+    } else if (angle < targetAngle) {//turn right
+      right();
+    }
+
+    //setting up propoertional control, see Step 3 on the website
+    if (abs(deltaAngle) > 30){
+      targetGyroZ = 60;
+    } else {
+      targetGyroZ = 2 * abs(deltaAngle);
+    }
+    
+    if (round(targetGyroZ - abs(GyroZ)) == 0){
+      ;
+    } else if (targetGyroZ > abs(GyroZ)){
+      leftSpeedVal = changeSpeed(leftSpeedVal, +1); //would increase abs(GyroX)
+    } else {
+      leftSpeedVal = changeSpeed(leftSpeedVal, -1);
+    }
+    rightSpeedVal = leftSpeedVal;
+    analogWrite(rightSpeed, rightSpeedVal);
+    analogWrite(leftSpeed, leftSpeedVal);
+  }
+}   
+
+int changeSpeed (int motorSpeed, int increment){
+  motorSpeed += increment;
+  if (motorSpeed > maxSpeed){ //to prevent motorSpeed from exceeding 255, which is a problem when using analogWrite
+    motorSpeed = maxSpeed;
+  } else if (motorSpeed < minSpeed){
+    motorSpeed = minSpeed;
+  }
+  return motorSpeed;
+}
+
+void forward(){ //drives the car forward, assuming leftSpeedVal and rightSpeedVal are set high enough
+  digitalWrite(right1, HIGH); //the right motor rotates FORWARDS when right1 is HIGH and right2 is LOW
+  digitalWrite(right2, LOW);
+  digitalWrite(left1, HIGH);
+  digitalWrite(left2, LOW);
+}
+
+void left(){ //rotates the car left, assuming speed leftSpeedVal and rightSpeedVal are set high enough
+  digitalWrite(right1, LOW);
+  digitalWrite(right2, HIGH);
+  digitalWrite(left1, HIGH);
+  digitalWrite(left2, LOW);
+}
+
+void right(){
+  digitalWrite(right1, HIGH);
+  digitalWrite(right2, LOW);
+  digitalWrite(left1, LOW);
+  digitalWrite(left2, HIGH);
+}
+
+void stopCar(){
+  digitalWrite(right1, LOW);
+  digitalWrite(right2, LOW);
+  digitalWrite(left1, LOW);
+  digitalWrite(left2, LOW);
+  analogWrite(rightSpeed, 0);
+  analogWrite(leftSpeed, 0);
+}
+
+
